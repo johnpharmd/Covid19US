ec0d2fb8e18f26ae7a71016c1d86998f7776366c
johnpharmd <jhumphre@uw.edu>
2020-03-29 13:05:49 -0700

    add renderUI/pickerInput; add filter logic to plot multiple lines

--- server.R
+++ server.R
@@ -4,7 +4,36 @@ library(jsonlite)
 library(dwapi)
 library(googleVis)
 library(ggplot2)
+library(shinyWidgets)
 library(dplyr)
+
+# Constants
+states_terrs <- c("Alabama", "Alaska", "Arizona", "Arkansas",
+                  "California", "Colorado",
+                  "Connecticut", "Delaware",
+                  "District of Columbia", "Florida",
+                  "Georgia", "Guam",
+                  "Hawaii", "Idaho",
+                  "Illinois", "Indiana",
+                  "Iowa", "Kansas",
+                  "Kentucky", "Louisiana",
+                  "Maine", "Maryland",
+                  "Massachusetts", "Michigan",
+                  "Minnesota", "Mississippi",
+                  "Missouri", "Montana",
+                  "Nebraska", "Nevada",
+                  "New Hampshire", "New Jersey",
+                  "New Mexico", "New York",
+                  "North Carolina", "North Dakota",
+                  "Ohio", "Oklahoma",
+                  "Oregon", "Pennsylvania",
+                  "Puerto Rico", "Rhode Island",
+                  "South Carolina", "South Dakota",
+                  "Tennessee", "Texas",
+                  "Utah", "Vermont",
+                  "Virgin Islands", "Virginia",
+                  "Washington", "West Virginia",
+                  "Wisconsin", "Wyoming")
 
 # Obtain data
 # awesome work by Hammerbacher et al.
@@ -26,7 +55,8 @@ sql_query <- "SELECT date, province_state, SUM(cases) AS number_of_cases
 tableau_jh_df <- data.frame(dwapi::sql(owner_id, dataset_id,
                                        sql_query))
 
-# Define server logic required to plot googlevis map and line plot
+# Define server logic required to plot googlevis map,
+# states/territories dropdown, and line plot
 shinyServer(function(input, output) {
 
     output$gvis <- renderGvis({
@@ -39,16 +69,25 @@ shinyServer(function(input, output) {
                      ))
     })
 
+    output$selections <- renderUI({
+        pickerInput("states_terrs", "Select up to 4 States/Territories:",
+                    states_terrs, list("max-options" = 4), multiple = TRUE)
+    })
+
+    upto4_filter <- reactive({
+        tableau_jh_df %>% filter(province_state %in% input$states_terrs)
+
+    })
+
     output$plot <- renderPlot({
-        # min_date <- as.Date("2020-01-20")
-        # max_date <- NA
-        ggplot(filter(tableau_jh_df, province_state == input$state_terr),
-                      aes(date, number_of_cases, group = 1)) +
+        upto4_filter = upto4_filter()
+        ggplot(upto4_filter, aes(date, number_of_cases, group = province_state,
+                                 col = province_state)) +
         geom_line() +
         labs(x = "Date", y = "Number of Cases") +
         theme(axis.text.x = element_text(angle = 45),
               axis.text.y = element_text(angle = 90),
-              plot.margin = unit(c(1, 1, 1, 1), "cm")) + 
+              plot.margin = unit(c(1.5, 1, 1, 1.5), "cm")) +
         scale_x_date(date_breaks = "weeks")
        })
 })
